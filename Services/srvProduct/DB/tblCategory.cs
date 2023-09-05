cf0c41163a013d8fb91ecf23a11c25888b89d83c
PRABHAKAR KUMAR SINGH <prabhakarks90@gmail.com>
2023-09-05 23:11:15 +0530

    Working on Category

--- Services/srvProduct/DB/tblCategory.cs
+++ Services/srvProduct/DB/tblCategory.cs
@@ -4,25 +4,31 @@ using srvProduct.protos;
 
 namespace srvProduct.DB
 {
+    public  abstract class BasicDetails
+    {
+        public string Language { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string? ShortDesc { get; set; }
+    }
+
     public class tblCategory
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? CategoryId { get; set; }
-        public bool IsActive { get; set; }
-        public string? ParentCategoryId { get; set; }
-        public string? RootCategoryId { get; set; }
-        public string Title { get; set; } = null!;
-        public string Name { get; set; } = null!;
-        public string? ShortDesc { get; set; }
-        public List<string> Keywords { get; set; } = null!;
-        public List<tblCategoryProperty> CategoryProperties { get; set; } = null!;
+        public bool IsActive { get; set; }     
+        public string? ImageUrl { get; set; }
+        public List<tblCategoryDetail> CategoryDetail { get; set; }
         public string? ModifiedBy { get; set; }
         public DateTime ModifiedDt { get; set; }
         public string? CreatedBy { get; set; }
         public DateTime CreatedDt { get; set; }
     }
-    public class tblCategoryProperty
+    public class tblCategoryDetail: BasicDetails
+    {   
+    }
+    public class tblProperty
     {
         public int PropertyDisplayOrder { get; set; }
         public string Name { get; set; } = null!;
@@ -32,4 +38,27 @@ namespace srvProduct.DB
         public string? Regx { get; set; }
         public List<string>? Option { get; set; }
     }
+
+    public class tblSubCategory
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? SubCategoryId { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? CategoryId { get; set; }
+        public bool IsActive { get; set; }
+        public string? ImageUrl { get; set; }
+        public List<tblSubCategoryDetails> SubCategoryDetail { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime ModifiedDt { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedDt { get; set; }
+    }
+
+    public class tblSubCategoryDetails : BasicDetails
+    {
+        public List<string> Keywords { get; set; } = null!;
+        public List<tblProperty> Properties { get; set; } = null!;
+    }
+
 }
